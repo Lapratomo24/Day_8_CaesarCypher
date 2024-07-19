f68440b322218e9dce63109e35e566b8f42a6aa8
Aditya <lapratomo24@yahoo.com>
2024-07-19 21:58:05 +0200

    add function to select between encode or decode

--- main.py
+++ main.py
@@ -31,21 +31,22 @@ def encrypt(text_input, shift_num):
     
     for letter in text_input:
         position = alphabet.index(letter)
-        new_position = position + shift_num
+        new_position = (position + shift_num) % 26
         new_letter = alphabet[new_position]
         encoded_text += new_letter
     print(f"The encoded text is {encoded_text}.")
         
-encrypt(text, shift)
-
 def decrypt(text_input, shift_amount):
     decoded_text = ""
     
     for letter in text_input:
         position = alphabet.index(letter)
-        new_position = position - shift_amount
+        new_position = (position - shift_amount) % 26
         new_letter = alphabet[new_position]
         decoded_text += new_letter
     print(f"The decoded text is {decoded_text}.")
 
-decrypt(text, shift)
+if direction == 'encode':
+    encrypt(text, shift)
+elif direction == 'decode':
+    decrypt(text, shift)
